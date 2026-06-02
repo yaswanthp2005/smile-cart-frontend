74ac6f9d73ce14ee87ec67e3463aadcdc639085d
Yaswanth Panatala <yashpanatala@gmail.com>
2026-06-02 17:04:33 +0530

    Integrated Ref to the Carousel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import { Left, Right } from "neetoicons";
 import { Button } from "neetoui";
 
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const timerRef = useRef(null);
+
   const handleNext = () => {
     setCurrentIndex(currentIndex => (currentIndex + 1) % imageUrls.length);
   };
@@ -13,12 +15,18 @@ const Carousel = ({ imageUrls, title }) => {
     setCurrentIndex(
       currentIndex => (currentIndex - 1 + imageUrls.length) % imageUrls.length
     );
+    resetTimer();
+  };
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = setInterval(handleNext, 3000);
   };
 
   useEffect(() => {
-    const interval = setInterval(handleNext, 3000);
+    timerRef.current = setInterval(handleNext, 3000);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(timerRef.current);
   }, []);
 
   return (
@@ -39,7 +47,10 @@ const Carousel = ({ imageUrls, title }) => {
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Right}
           style="text"
-          onClick={handleNext}
+          onClick={() => {
+            handleNext();
+            resetTimer();
+          }}
         />
       </div>
       <div className="mt-3 flex space-x-1">
@@ -55,7 +66,10 @@ const Carousel = ({ imageUrls, title }) => {
                   ? `${defaultClasses} bg-black`
                   : defaultClasses
               }
-              onClick={() => setCurrentIndex(index)}
+              onClick={() => {
+                setCurrentIndex(index);
+                resetTimer();
+              }}
             />
           );
         })}
